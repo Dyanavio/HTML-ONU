7f729328d651b5806d4f7d82e2374d4806435f15
Dyanavio <dimant2018@gmail.com>
2025-03-13 09:14:13 +0000

    Minor changes

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 5050;
+const path = require('path');
 
+app.use(express.static(path.join(__dirname)))
+//app.use(express.static('.'));
 
 app.get('/api', (req, res) => 
 {
-    res.send('Hello API World from Server!');
+    res.send('You\' gone too far');
 });
 
 app.get('/', (req, res) =>
 {
-    res.sendFile(__dirname + '/mtable.html');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/page2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'page2.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => 
